d32ce012c4be8ea319d56e8311a50d845ed32b7b
Twist <em@wi.com.ua>
2022-12-19 00:15:13 +0200

    updated main.py and tests.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('sample')
+def progression(start, d, n):
+    if n < 0:
+        raise ValueError
+    return (n / 2) * (2 * start + (n - 1) * d)
+
+
+if __name__ == "__main__":
+    print("Мєзєнцев Єгор Миколайович")
+
+    n = int(input("Введите кол-во: "))
+    d = int(input("Введите розность: "))
+    start = int(input("Введите начальное значение: "))
+
+    print(progression(start, d, n))
--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-print('sample')
+import unittest
+
+from main import progression
+
+class TestMyApp(unittest.TestCase):
+    def test_progression(self):
+        start, d, n = 2, 3, 3
+        self.assertEqual(progression(start, d, n), 15.0)
+
+        start, d, n = 2, -3, 3
+        self.assertEqual(progression(start, d, n), -3.0)
+
+        start, d, n = 0, 0, 0
+        self.assertEqual(progression(start, d, n), 0.0)
+
+        start, d, n = 2, 3, -3
+        with self.assertRaises(ValueError):
+            progression(start, d, n)
+
+        start, d, n = "2", 3, 2
+        with self.assertRaises(TypeError):
+            progression(start, d, n)
