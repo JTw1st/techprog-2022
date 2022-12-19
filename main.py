6e39997ff3d464be8a79347db3214c0e64da72b1
Twist <em@wi.com.ua>
2022-12-19 10:37:27 +0200

    updated main.py and tests.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-def progression(start, d, n):
-    if n < 0:
+def progression(n, start=5, d=3):
+    if n <= 0:
         raise ValueError
-    return (n / 2) * (2 * start + (n - 1) * d)
+    return start + (n - 1) * d
 
 
 if __name__ == "__main__":
     print("Мєзєнцев Єгор Миколайович")
 
-    n = int(input("Введите кол-во: "))
-    d = int(input("Введите розность: "))
-    start = int(input("Введите начальное значение: "))
+    n = int(input("Введіть номер n-того елементу: "))
 
-    print(progression(start, d, n))
+    print(progression(n))
--- tests.py
+++ tests.py
@@ -4,19 +4,19 @@ from main import progression
 
 class TestMyApp(unittest.TestCase):
     def test_progression(self):
-        start, d, n = 2, 3, 3
-        self.assertEqual(progression(start, d, n), 15.0)
+        n = 3
+        self.assertEqual(progression(n), 11.0)
 
-        start, d, n = 2, -3, 3
-        self.assertEqual(progression(start, d, n), -3.0)
+        n, start, d = 3, 5, 3
+        self.assertEqual(progression(n, start, d), 11.0)
 
-        start, d, n = 0, 0, 0
-        self.assertEqual(progression(start, d, n), 0.0)
+        n, start, d = 5, 5, 5
+        self.assertEqual(progression(n, start, d), 25.0)
 
-        start, d, n = 2, 3, -3
+        n = -3
         with self.assertRaises(ValueError):
-            progression(start, d, n)
+            progression(n, start, d)
 
-        start, d, n = "2", 3, 2
+        n = "2"
         with self.assertRaises(TypeError):
-            progression(start, d, n)
+            progression(n, start, d)
